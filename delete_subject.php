fcb2a40345006148caca1b52fdce5087d9598b4e
ryantate7 <ryantate7@yahoo.com>
2013-06-17 14:09:43 -0400

    Create delete_subject.php

---
+++ delete_subject.php
@@ -0,0 +1,35 @@
+<?php require_once("includes/session.php");?>
+<?php require_once("includes/connection.php")?>
+<?php require_once("includes/functions.php")?>
+<?php  confirm_logged_in();//will redirect them to the log-in screen if they are not logged in?>
+<?php
+  if(intval($_GET['subj'])==0)
+	{
+		redirect_to("content.php");
+	}
+	
+	$id = mysql_prep($_GET['subj']);
+	
+	if($subject = get_subject_by_id($id))
+	{
+		$query = "DELETE FROM subjects WHERE id = {$id} LIMIT 1";
+		$result = mysql_query($query, $connection);
+	
+		if(mysql_affected_rows()==1)
+		{//success
+			redirect_to("content.php");
+		}
+		else
+		{//deletion Failed
+			echo "<p>Subject deletion failed!</p>";
+			echo "<p>" . mysql_error() . "</p>";
+			echo "<a href=\"content.php\">Return to Main Page</a>";
+		}
+	}
+	else
+	{	//subject didn't exist in Database
+		redirect_to("content.php");
+		
+	}
+?>
+<?php mysql_close($connection); ?>
