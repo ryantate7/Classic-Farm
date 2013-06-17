d1ff41ddcc7b727f1bee4e35fee57dfc2d9de9f8
ryantate7 <ryantate7@yahoo.com>
2013-06-17 16:08:54 -0400

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+		<title>Classic Farm</title>
+		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="header">
+			<h1>Classic Farm</h1>
+		</div>
+		<div id="main">
