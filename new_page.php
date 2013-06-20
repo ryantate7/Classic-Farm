04309f5af2250a16520f41d702f703893a3c4ecd
ryantate7 <ryantate7@yahoo.com>
2013-06-20 14:42:39 -0500

    Update new_page.php
    Removed all table tags.

--- new_page.php
+++ new_page.php
@@ -1,15 +1,16 @@
+<?php require_once("includes/session.php");?>
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php  confirm_logged_in();//will redirect them to the log-in screen if they are not logged in ?>
 <?php find_selected_page();//figure out what has been chosen ?>
 <?php include("includes/header.php");?>
 
-<table id="structure">
-  <tr>
-		<td id="navigation">
+<div id="structure">
+		<div id="navigation">
 			<?php echo navigation($sel_subject, $sel_page); ?>
-		</td>
-		<td id="page">
+		</div>
+		
+		<div id="page">
 			<h2>Add Page to Subject-<?php echo $sel_subject['menu_name'];?></h2>
 			<?php 
 				if(!empty($message))
@@ -28,11 +29,10 @@
 			<form action="create_page.php?subj=<?php echo urlencode($sel_subject['id']);?>" method="post">
 				<?php $new_page = true;?>	
 				<?php include("page_form.php");?>
-				<input type="submit" name="submit" value="Add Page" />
+				<div class="actions"><input type="submit" name="submit" value="Add Page" /></div>
 			</form>
 			<br>
 			<a href="content.php">Cancel</a>
-		</td>
-	</tr>
-</table>
+		</div>
+</div>
 <?php require("includes/footer.php")?>
