c87a637fedb0f293c699fc7437340e6d541b6303
ryantate7 <ryantate7@yahoo.com>
2013-06-20 15:43:04 -0400

    Update new_subject.php

--- new_subject.php
+++ new_subject.php
@@ -1,41 +1,36 @@
+<?php require_once("includes/session.php");?>
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
-<?php  confirm_logged_in();//will redirect them to the log-in screen if they are not logged in ?>
-<?php find_selected_page();//figure out what has been chosen ?>
+<?php confirm_logged_in();//will redirect them to the log-in screen if they are not logged in ?>
 <?php include("includes/header.php");?>
 
-<table id="structure">
-  <tr>
-		<td id="navigation">
+<div id="structure">
+		<div id="navigation">
 			<?php echo navigation($sel_subject, $sel_page); ?>
-		</td>
-		<td id="page">
+		</div>
+		<div id="page">
 			<h2>Add Subject</h2>
+			<?php 
+				if(!empty($message))
+				{
+					echo "<p class=\"message\">" . $message . "</p>";
+				}	
+			?>
+			<?php
+				//output the list of the fields that had errors
+				if(!empty($errors))
+				{
+					display_errors($errors);
+				}
+			?>
+			
 			<form action="create_subject.php" method="post">
-				<p>Subject Name: <input type="text" name="menu_name" value="" id="menu_name" /></p>
-					<p>Position:
-						<select name="position">
-							<?php 
-								$item_set = get_all_items();
-								$item_count = mysql_num_rows($item_set);
-								for($count=1; $count <= $item_count+1; $count++)
-								{
-									echo "<option value=\"{$count}\">{$count}</option>";
-								
-								}
-							?>	
-						</select>
-					</p>
-					<p>Visible:
-						<input type="radio" name="visible" value="0" />No
-						&nbsp;
-						<input type="radio" name="visible" value="1" />Yes
-					</p>
-					<input type="submit" value="Add Item" />
+			<?php $new_subject = true;?>
+			<?php include("subject_form.php");?>
+			<div class="actions"><input type="submit" name="submit" value="Add Subject" /></div>
 			</form>
 			<br>
 			<a href="content.php">Cancel</a>
-		</td>
-	</tr>
-</table>
+		</div>
+</div>
 <?php require("includes/footer.php")?>
